05bca02b489adadf10c5fcf586620006e01d4b7a
Huyen <huyen@local>
2026-07-21 15:01:55 -0500

    Bumped service worker cache to v11

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline PWA Service Worker - Network-First with Cache Fallback Caching Strategy */
 
-const CACHE_NAME = 'dbt-companion-v10';
+const CACHE_NAME = 'dbt-companion-v11';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
